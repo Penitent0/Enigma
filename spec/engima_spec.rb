0cbfa3f4d0664823297809d7b6040aea7a264b69
Ken Lenhart <omnipaid@gmail.com>
2022-08-05 15:13:14 -0700

    Add date instance variable date_to_string method

--- spec/engima_spec.rb
+++ spec/engima_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Enigma do
     it 'instantiates' do
       expect(enigma).to be_a(Enigma)
     end
+
+    it 'has date instance variable and date to string method' do
+      expect(enigma.date).to be_a(Time)
+      expect(enigma.date_to_string).to be_a(String)
+      expect(enigma.date_to_string.length).to eq(6)
+    end
   end
 end
