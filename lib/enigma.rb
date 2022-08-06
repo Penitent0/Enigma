0dfaae267d0eda95ba6be4a1ca2ecc8ff83d1ab1
Ken Lenhart <omnipaid@gmail.com>
2022-08-06 15:16:12 -0700

    Add alphabet_generator, offset_generator, message_format methods and tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -16,7 +16,33 @@ class Enigma
     @date.strftime('%d/%m/%y').gsub('/', '')
   end
 
-  def encrypt(message, key, date)
+  def alphabet_generator
+    ("a".."z").to_a << " "
+  end
 
+  def offset_generator(key, date)
+    a_shift = (key[0..1].to_i + (date.to_i ** 2).to_s[-4..-1][0].to_i)
+    b_shift = (key[1..2].to_i + (date.to_i ** 2).to_s[-4..-1][1].to_i)
+    c_shift = (key[2..3].to_i + (date.to_i ** 2).to_s[-4..-1][2].to_i)
+    d_shift = (key[3..4].to_i + (date.to_i ** 2).to_s[-4..-1][3].to_i)
+    offsets_array = [a_shift, b_shift, c_shift, d_shift]
+  end
+
+  def message_format(message)
+    message.downcase.split('')
+  end
+
+  def encrypt(message, key = key_generator, date = date_to_string)
+    offset_enumerator = offset_generator(key, date).cycle
+    letter_array = message_format(message)
+    encrypted_message = ""
+    letter_array.each do |letter|
+      if alphabet_generator.include?(letter) == false
+        encrypted << letter
+      else
+        encrypted << abc.rotate(abc.find_index(letter) + offset_enumerator.next).first
+      end
+    end
+    encryption_hash = {encryption: encrypted_message, key: key, date: date}
   end
 end
--- spec/engima_spec.rb
+++ spec/engima_spec.rb
@@ -22,9 +22,23 @@ RSpec.describe Enigma do
       expect(key_sum).to be >= 0
     end
 
-    it 'has encrypt method' do
+    it 'has message format method' do
+      expect(enigma.message_format("hello world")).to be_a(Array)
+      expect(enigma.message_format("hello world")).to eq(["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"])
+    end
+
+    it 'has offset generator method' do
+      expect(enigma.offset_generator("02715", "040895")).to be_a(Array)
+      expect(enigma.offset_generator("02715", "040895")[0]).to eq(3)
+      expect(enigma.offset_generator("02715", "040895")[1]).to eq(27)
+      expect(enigma.offset_generator("02715", "040895")[2]).to eq(73)
+      expect(enigma.offset_generator("02715", "040895")[3]).to eq(20)
+    end
+
+    xit 'has encrypt method' do
       expect(enigma.encrypt("hello world", "02715", "040895")).to be_a(Hash)
       expect(enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+      #add tests of optional args here
     end
   end
 end
