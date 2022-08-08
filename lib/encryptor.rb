00bc3b659035a41eede1d7aa5dd4db74ea5fd1db
Ken Lenhart <omnipaid@gmail.com>
2022-08-07 21:15:50 -0700

    Add instance variables, attr_reader for tests

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,5 +1,10 @@
 class Encryptor
-  def initialize
-
+  attr_reader :message,
+              :key,
+              :date
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
   end
 end 
