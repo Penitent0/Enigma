34fcb8edff301f44619ff11971466c460ce03b75
Ken Lenhart <omnipaid@gmail.com>
2022-08-09 10:26:41 -0700

    Add decrypt method to decryptor from enigma class

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -7,4 +7,21 @@ include Encryptable
   def initialize
     @decrypted = nil
   end
+
+  def decrypt(message, key, date = date_to_string)
+    if message.empty? == true 
+      return "Message must contain content"
+    end
+    offset_enumerator = offset_generator(key, date).cycle
+    letter_array = message_format(message)
+    decrypted_message = ""
+    letter_array.each do |letter|
+      if alphabet_generator.include?(letter) == false
+        decrypted_message << letter
+      else
+        decrypted_message << alphabet_generator.rotate(alphabet_generator.find_index(letter) - offset_enumerator.next).first
+      end
+    end
+    @decrypted = {decryption: decrypted_message, key: key, date: date}
+  end
 end
