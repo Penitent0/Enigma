53780fce4eeb0fa643d46473bc7a828588ef6f33
Ken Lenhart <omnipaid@gmail.com>
2022-08-08 17:59:40 -0700

    Refactor tests for encryptor.rb enigma.rb

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 
 RSpec.describe Encryptor do 
   context 'when an encryptor is created it' do
-    let!(:encryptor) { Encryptor.new("hello world", "02715", "040895") { extend Encryptable } }
+    let!(:encryptor) { Encryptor.new { extend Encryptable } }
+
     it 'instantiates' do
       expect(encryptor).to be_a(Encryptor)
     end
 
-    it 'has details message, key, date' do
-      expect(encryptor.message).to eq("hello world")
-      expect(encryptor.key).to eq("02715")
-      expect(encryptor.date).to eq("040895")
-    end
+    # it 'has details message, key, date' do
+    #   expect(encryptor.message).to eq("hello world")
+    #   expect(encryptor.key).to eq("02715")
+    #   expect(encryptor.date).to eq("040895")
+    # end
 
     it 'extends encryptable key generator' do
       expect(encryptor.key_generator).to be_a(String)
